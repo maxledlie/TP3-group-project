196189f509f39ed51b89d44e1c4fdd3977462576
maxledlie <maxledlie@hotmail.co.uk>
2016-02-10 09:20:19 +0000

    Board.cpp now supports bitmap initialisation

--- framework/Board.cpp
+++ framework/Board.cpp
@@ -1,3 +1,4 @@
+#include "bitmap_image.hpp"
 #include "stringmanip.h"
 #include <tuple>
 #include <string>
@@ -11,14 +12,13 @@ using namespace std;
 class Board
 {
 public:
-	static const int GRID_WIDTH = 101;
-	static const int GRID_HEIGHT = 101;
-	Board(void); // default constructor
+	vector<vector<tuple<double, bool>>> grid;
+
+	void bmpInitialise(string bmp_file_name);
 	void jacobiUpdate();
 	void writeBoard(string ofile_name);
-	void textInitialise();
 
-	vector<vector<tuple<double,bool>>> grid;
+
 
 };
 
@@ -26,72 +26,59 @@ public:
 
 /*****************************************************************************************************************************************/
 // Default constructor:
-// Initialise the grid to be an empty grid of the given dimensions.
-Board::Board(void) {
-	for (size_t i = 0; i < GRID_WIDTH; ++i) {
+// Initialise the grid based on the provided .bmp image. (RED = +5V, GREEN = 0V, BLUE = -5V)
+void Board::bmpInitialise(string bmp_file_name) {
+
+	bitmap_image image(bmp_file_name);
+
+	if (!image){
+		printf("Error: Failed to open input.bmp\n");
+	}
+
+	unsigned char red;
+	unsigned char green;
+	unsigned char blue;
+
+	int grid_width = image.width();
+	int grid_height = image.height();
+
+	for (int i = 0; i < grid_width; ++i) {
 		grid.push_back(vector<tuple<double, bool>>());
 	}
-	for (size_t i = 0; i < GRID_WIDTH; ++i) {
-		for (size_t j = 0; j < GRID_HEIGHT; ++j) {
+	for (int i = 0; i < grid_width; ++i) {
+		for (int j = 0; j < grid_height; ++j) {
 			grid[i].push_back(make_tuple(0, false));
 		}
 	}
-};
 
-
-// Initialises a board from a text file
-void Board::textInitialise() {
-
-	ifstream shapes_file;
-	shapes_file.open("shapes.txt");
-	string s;
-	// For each rectangle
-	while (getline(shapes_file, s)){
-		cout << "Reading a line... " << endl; // Test
-
-		// Break the line into its constituent ints, representing the properties of the rectangle
-		vector<int> properties = split(s);
-		vector<int>::const_iterator iter = properties.begin();
-		size_t origin_x = *iter;
-		cout << "The x-origin is " << origin_x << endl; // Test
-		++iter;
-		size_t origin_y = *iter;
-		cout << "The y-origin is " << origin_y << endl; // Test
-		++iter;
-		size_t width = *iter;
-		cout << "The width is " << width << endl; // Test
-		++iter;
-		size_t height = *iter;
-		cout << "The height is " << height << endl; // Test
-		++iter;
-		int potential = *iter;
-
-		// Check if the rectangle will fit on the board
-		if (origin_x < GRID_WIDTH && origin_x + width <= GRID_WIDTH && origin_y < GRID_HEIGHT && origin_y + height <= GRID_HEIGHT) {
-			// Enter the given potential at every point on the grid that lies within the rectangle.
-			// For each x-coord within the rectangle
-			for (size_t i = origin_x; i < origin_x + width; ++i) {
-				// For each y-coord within the rectangle
-				for (size_t j = origin_y; j < origin_y + height; ++j) {
-					// Set the potential of this cell and mark it as permanent.
-					grid[i][j] = make_tuple(potential,true);
-
-				}
-			}
+	for (int x = 0; x < grid_width; ++x) {
+		for (int y = 0; y < grid_height; ++y) {
+			image.get_pixel(x, y, red, green, blue);
+			if (red == 255 && green == 0 && blue == 0)
+				grid[x][y] = make_tuple(5, true);
+			else if (red == 0 && green == 255 && blue == 0)
+				grid[x][y] = make_tuple(0, true);
+			else if (red == 0 && green == 0 && blue == 255)
+				grid[x][y] = make_tuple(-5, true);
 		}
 	}
-	shapes_file.close();
+
+	return;
 }
+
+
 
 
 // Performs one iteration of the Jacobi method of relaxation. SLOW! ***************************************************************************************
 void Board::jacobiUpdate() {
 
+	int grid_width = grid.size();
+	int grid_height = grid[0].size();
+
 	// Make a copy of the grid
-	vector<vector<tuple<double, bool>>> new_grid = grid;;
-	//copy(&grid[0][0], &grid[0][0] + GRID_WIDTH*GRID_HEIGHT, &new_grid[0][0]);
-	for (size_t i = 0; i < GRID_WIDTH; ++i) {
-		for (size_t j = 0; j < GRID_HEIGHT; ++j) {
+	vector<vector<tuple<double, bool>>> new_grid = grid;
+	for (int i = 0; i < grid_width; ++i) {
+		for (int j = 0; j < grid_height; ++j) {
 			new_grid[i][j] = grid[i][j];
 		}
 	}
@@ -101,63 +88,66 @@ void Board::jacobiUpdate() {
 	if (get<1>(grid[0][0]) == false)				// Top-left
 		new_grid[0][0] = tuple<double, bool>(0.5*(get<0>(grid[0][1]) + get<0>(grid[1][0])), false);
 
-	if (get<1>(grid[0][GRID_HEIGHT-1]) == false)	// Bottom-left
-		new_grid[0][GRID_HEIGHT-1] = tuple<double, bool>(0.5*(get<0>(grid[0][GRID_HEIGHT - 2]) + get<0>(grid[1][GRID_HEIGHT - 1])), false);
+	if (get<1>(grid[0][grid_height-1]) == false)	// Bottom-left
+		new_grid[0][grid_height-1] = tuple<double, bool>(0.5*(get<0>(grid[0][grid_height - 2]) + get<0>(grid[1][grid_height - 1])), false);
 
-	if (get<1>(grid[GRID_WIDTH - 1][0]) == false)		// Top-right
-		new_grid[GRID_WIDTH - 1][0] = tuple<double, bool>(0.5*(get<0>(grid[GRID_WIDTH - 2][0]) + get<0>(grid[GRID_WIDTH - 1][1])), false);
+	if (get<1>(grid[grid_width - 1][0]) == false)		// Top-right
+		new_grid[grid_width - 1][0] = tuple<double, bool>(0.5*(get<0>(grid[grid_width - 2][0]) + get<0>(grid[grid_width - 1][1])), false);
 
-	if (get<1>(grid[GRID_WIDTH - 1][GRID_HEIGHT - 1]) == false)	// Bottom-right
-		new_grid[GRID_WIDTH - 1][GRID_HEIGHT - 1] = tuple<double, bool>(0.5*(get<0>(grid[GRID_WIDTH - 2][GRID_HEIGHT - 1]) + get<0>(grid[GRID_WIDTH - 1][GRID_HEIGHT - 2])), false);
+	if (get<1>(grid[grid_width - 1][grid_height - 1]) == false)	// Bottom-right
+		new_grid[grid_width - 1][grid_height - 1] = tuple<double, bool>(0.5*(get<0>(grid[grid_width - 2][grid_height - 1]) + get<0>(grid[grid_width - 1][grid_height - 2])), false);
 
 
 	// Next, update the edge pieces:
 	// Top edge
-	for (size_t i = 1; i < GRID_WIDTH - 1; ++i) {
+	for (int i = 1; i < grid_width - 1; ++i) {
 		if (get<1>(grid[i][0]) == false)
 			new_grid[i][0] = tuple<double, bool>((get<0>(grid[i - 1][0]) + get<0>(grid[i + 1][0]) + get<0>(grid[i][1])) / 3, false);
 	}
 	// Bottom edge
-	for (size_t i = 1; i < GRID_WIDTH - 1; ++i) {
-		if (get<1>(grid[i][GRID_HEIGHT - 1]) == false)
-			new_grid[i][GRID_HEIGHT - 1] = tuple<double, bool>((get<0>(grid[i - 1][GRID_HEIGHT - 1]) + get<0>(grid[i + 1][GRID_HEIGHT - 1]) + get<0>(grid[i][GRID_HEIGHT - 2])) / 3, false);
+	for (int i = 1; i < grid_width - 1; ++i) {
+		if (get<1>(grid[i][grid_height - 1]) == false)
+			new_grid[i][grid_height - 1] = tuple<double, bool>((get<0>(grid[i - 1][grid_height - 1]) + get<0>(grid[i + 1][grid_height - 1]) + get<0>(grid[i][grid_height - 2])) / 3, false);
 	}
 	// Left edge
-	for (size_t j = 1; j < GRID_HEIGHT - 1; ++j) {
+	for (int j = 1; j < grid_height - 1; ++j) {
 		if (get<1>(grid[0][j]) == false)
 			new_grid[0][j] = tuple<double, bool>((get<0>(grid[0][j - 1]) + get<0>(grid[0][j + 1]) + get<0>(grid[1][j])) / 3, false);
 	}
 	// Right edge
-	for (size_t j = 1; j < GRID_HEIGHT - 1; ++j) {
-		if (get<1>(grid[GRID_WIDTH - 1][j]) == false)
-			new_grid[GRID_WIDTH - 1][j] = tuple<double, bool>((get<0>(grid[GRID_WIDTH - 1][j - 1]) + get<0>(grid[GRID_WIDTH - 1][j + 1]) + get<0>(grid[GRID_WIDTH - 2][j])) / 3, false);
+	for (int j = 1; j < grid_height - 1; ++j) {
+		if (get<1>(grid[grid_width - 1][j]) == false)
+			new_grid[grid_width - 1][j] = tuple<double, bool>((get<0>(grid[grid_width - 1][j - 1]) + get<0>(grid[grid_width - 1][j + 1]) + get<0>(grid[grid_width - 2][j])) / 3, false);
 	}
 
 	// Finally, update the interior cells
-	for (size_t i = 1; i < GRID_WIDTH - 1; ++i) {
-		for (size_t j = 1; j < GRID_HEIGHT - 1; ++j) {
+	for (int i = 1; i < grid_width - 1; ++i) {
+		for (int j = 1; j < grid_height - 1; ++j) {
 			if (get<1>(grid[i][j]) == false)
 				new_grid[i][j] = tuple<double, bool>((get<0>(grid[i][j - 1]) + get<0>(grid[i - 1][j]) + get<0>(grid[i + 1][j]) + get<0>(grid[i][j + 1])) / 4, false);
 		}
 	}
 
 	// grid = new_grid
-	//copy(&new_grid[0][0], &new_grid[0][0] + GRID_WIDTH*GRID_HEIGHT, &grid[0][0]);
-	for (size_t i = 0; i < GRID_WIDTH; ++i) {
-		for (size_t j = 0; j < GRID_HEIGHT; ++j) {
+	//copy(&new_grid[0][0], &new_grid[0][0] + grid_width*grid_height, &grid[0][0]);
+	for (int i = 0; i < grid_width; ++i) {
+		for (int j = 0; j < grid_height; ++j) {
 			grid[i][j] = new_grid[i][j];
 		}
 	}
 }
 
-/**************************************************************************************************************************************************/
 	
-// Writes the current grid to a given output file so it can be plotted
+// Writes the current grid to a given output file so it can be plotted *************************************************************************************
 void Board::writeBoard(string ofilename) {
-	ofstream ofile;
+
+	int grid_width = grid.size();
+	int grid_height = grid[0].size();
+ 	ofstream ofile;
+	cout << "Writing output to " << ofilename << endl;
 	ofile.open(ofilename);
-	for (size_t i = 0; i < GRID_WIDTH; ++i) {
-		for (size_t j = 0; j < GRID_HEIGHT; ++j) {
+	for (int i = 0; i < grid_width; ++i) {
+		for (int j = 0; j < grid_height; ++j) {
 			ofile << i << " " << j << " " << get<0>(grid[i][j]) << endl;
 		}
 		ofile << endl;
