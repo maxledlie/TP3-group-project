828085920fec9cb26e7c9f39bb9b93ed48f77c08
maxledlie <maxledlie@hotmail.co.uk>
2016-02-10 09:24:30 +0000

    Update Source.cpp

--- framework/Source.cpp
+++ framework/Source.cpp
@@ -1,43 +1,37 @@
 #include "board.h"
+#include "bitmap_image.hpp"
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
-#include <algorithm>
 #include <tuple>
-#include <cstddef>
+
 
 using namespace std;
 
 
-
-
 int main()
 {
-	int iterations;
-	string shapes_file_name;
+	string bmp_file_name;
+	string out_file_name;
+	unsigned int iterations;
 
 	cout << "Welcome to the E-Field Solver!" << endl << endl;
-	cout << "How many iterations would you like to use? " << endl;
+	cout << "Enter the name of the bitmap file to use as input: " << endl;
+	cin >> bmp_file_name;
+	cout << "Enter the name of the file to output the data to: " << endl;
+	cin >> out_file_name;
+	cout << "Enter the number of iterations to use: " << endl;
 	cin >> iterations;
 
-	// Create a new board, initialised according to the given shapes file
+	// Create a new board initialised according to the provided .bmp file
 	Board board;
-	cout << "Created a new board" << endl;	// Test
-	board.textInitialise();
-	cout << "Ok, I've initialised the board." << endl; // Test
-	for (int i = 0; i < iterations; ++i) {
+	board.bmpInitialise(bmp_file_name);
+	cout << "Initialised the board" << endl; // Test
+
+	for (int i = 0; i < iterations; ++i)
 		board.jacobiUpdate();
-	}
-	cout << "I've run the simulation" << endl; // Test
-	board.writeBoard("out.txt");
-	cout << "I've written the board to out.txt" << endl; // Test
 
-	exit(0);
+	board.writeBoard(out_file_name);
+	return 0;
 }
-
-
-
-
-/* Format of shapes file:
-	origin_x origin_y width height potential */
