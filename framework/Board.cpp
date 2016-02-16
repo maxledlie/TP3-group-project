9ebd311893250ace9bfa17ef2d1e8ee4a9e1c631
maxledlie <maxledlie@hotmail.co.uk>
2016-02-16 11:40:32 +0000

    Update Board.cpp
    Added Max's Gaussian method and Wenbo's SOR method. Both seem to work well, with Jacobi < Gauss < SOR in terms of speed. Updated all methods to work with Lauren's upcoming converged() function.

--- framework/Board.cpp
+++ framework/Board.cpp
@@ -12,10 +12,13 @@ class Board
 {
 public:
 	vector<vector<tuple<double, bool>>> grid;
+	vector<vector<tuple<double, bool>>> new_grid;
 
 	void bmpInitialise(string bmp_file_name);
+	bool converged();
 	void jacobiUpdate();
 	void gaussUpdate();
+	void sorUpdate();
 	void writeBoard(string ofile_name);
 
 
@@ -63,25 +66,28 @@ void Board::bmpInitialise(string bmp_file_name) {
 		}
 	}
 
+	new_grid = grid;
+
 	return;
 }
 
 
 
+// TODO Check if the simulation has converged - that is, if 'new_grid' is sufficiently similar to 'grid' ***************************************************
+bool Board::converged() {
+	return false;
+}
+
 
 // Performs one iteration of the Jacobi method of relaxation. SLOW! ***************************************************************************************
+// After this method is run, new_grid holds values that are one iteration further along than 'grid'
 void Board::jacobiUpdate() {
+
+	grid = new_grid;
 
 	int grid_width = grid.size();
 	int grid_height = grid[0].size();
 
-	// Make a copy of the grid
-	vector<vector<tuple<double, bool>>> new_grid = grid;
-	for (int i = 0; i < grid_width; ++i) {
-		for (int j = 0; j < grid_height; ++j) {
-			new_grid[i][j] = grid[i][j];
-		}
-	}
 		
 	// First update the corner pieces:
 
@@ -129,71 +135,117 @@ void Board::jacobiUpdate() {
 	}
 }
 
-    void Board::gaussUpdate() {
-        
-        int grid_width = grid.size();
-        int grid_height = grid[0].size();
-        
-        // Make a copy of the grid
-        vector<vector<tuple<double, bool>>> new_grid = grid;
-        for (int i = 0; i < grid_width; ++i) {
-            for (int j = 0; j < grid_height; ++j) {
-                new_grid[i][j] = grid[i][j];
-            }
-        }
-        
-        // First update the corner pieces:
-        
-        if (get<1>(grid[0][0]) == false)				// Top-left
-            new_grid[0][0] = tuple<double, bool>(0.5*(get<0>(grid[0][1]) + get<0>(grid[1][0])), false);
-        	grid[0][0]=new_grid[0][0];
 
-        if (get<1>(grid[0][grid_height-1]) == false)	// Bottom-left
-            new_grid[0][grid_height-1] = tuple<double, bool>(0.5*(get<0>(grid[0][grid_height - 2]) + get<0>(grid[1][grid_height - 1])), false);
-		grid[0][grid_height-1]=new_grid[0][grid_height-1];
-        
-        if (get<1>(grid[grid_width - 1][0]) == false)		// Top-right
-            new_grid[grid_width - 1][0] = tuple<double, bool>(0.5*(get<0>(grid[grid_width - 2][0]) + get<0>(grid[grid_width - 1][1])), false);
-		grid[grid_width-1][0]=new_grid[grid_width-1][0];
-        
-        if (get<1>(grid[grid_width - 1][grid_height - 1]) == false)	// Bottom-right
-            new_grid[grid_width - 1][grid_height - 1] = tuple<double, bool>(0.5*(get<0>(grid[grid_width - 2][grid_height - 1]) + get<0>(grid[grid_width - 1][grid_height - 2])), false);
-		grid[grid_width-1][grid_height-1]=new_grid[grid_width-1][grid_height-1];        
-        
-        // Next, update the edge pieces:
-        // Top edge
-        for (int i = 1; i < grid_width - 1; ++i) {
-            if (get<1>(grid[i][0]) == false)
-                new_grid[i][0] = tuple<double, bool>((get<0>(grid[i - 1][0]) + get<0>(grid[i + 1][0]) + get<0>(grid[i][1])) / 3, false);
-        	grid[i][0]=new_grid[i][0];
+// Performs one iteration of the Gaussian simulation ******************************************************************************************************
+// Author: Max S. Modified to work with converged().
+void Board::gaussUpdate() {
+
+	int grid_width = grid.size();
+	int grid_height = grid[0].size();
+
+	grid = new_grid;
+
+	// First update the corner pieces:
+
+	if (get<1>(new_grid[0][0]) == false)				// Top-left
+		new_grid[0][0] = tuple<double, bool>(0.5*(get<0>(new_grid[0][1]) + get<0>(new_grid[1][0])), false);
+
+	if (get<1>(new_grid[0][grid_height - 1]) == false)	// Bottom-left
+		new_grid[0][grid_height - 1] = tuple<double, bool>(0.5*(get<0>(new_grid[0][grid_height - 2]) + get<0>(new_grid[1][grid_height - 1])), false);
+
+	if (get<1>(new_grid[grid_width - 1][0]) == false)		// Top-right
+		new_grid[grid_width - 1][0] = tuple<double, bool>(0.5*(get<0>(new_grid[grid_width - 2][0]) + get<0>(new_grid[grid_width - 1][1])), false);
+
+	if (get<1>(new_grid[grid_width - 1][grid_height - 1]) == false)	// Bottom-right
+		new_grid[grid_width - 1][grid_height - 1] = tuple<double, bool>(0.5*(get<0>(new_grid[grid_width - 2][grid_height - 1]) + get<0>(new_grid[grid_width - 1][grid_height - 2])), false);
+
+	// Next, update the edge pieces:
+	// Top edge
+	for (int i = 1; i < grid_width - 1; ++i) {
+		if (get<1>(new_grid[i][0]) == false)
+			new_grid[i][0] = tuple<double, bool>((get<0>(new_grid[i - 1][0]) + get<0>(new_grid[i + 1][0]) + get<0>(new_grid[i][1])) / 3, false);
 	}
-        // Bottom edge
-        for (int i = 1; i < grid_width - 1; ++i) {
-            if (get<1>(grid[i][grid_height - 1]) == false)
-                new_grid[i][grid_height - 1] = tuple<double, bool>((get<0>(grid[i - 1][grid_height - 1]) + get<0>(grid[i + 1][grid_height - 1]) + get<0>(grid[i][grid_height - 2])) / 3, false);
-        	grid[i][grid_height-1]=new_grid[i][grid_height-1];
+	// Bottom edge
+	for (int i = 1; i < grid_width - 1; ++i) {
+		if (get<1>(new_grid[i][grid_height - 1]) == false)
+			new_grid[i][grid_height - 1] = tuple<double, bool>((get<0>(new_grid[i - 1][grid_height - 1]) + get<0>(new_grid[i + 1][grid_height - 1]) + get<0>(new_grid[i][grid_height - 2])) / 3, false);
 	}
-        // Left edge
-        for (int j = 1; j < grid_height - 1; ++j) {
-            if (get<1>(grid[0][j]) == false)
-                new_grid[0][j] = tuple<double, bool>((get<0>(grid[0][j - 1]) + get<0>(grid[0][j + 1]) + get<0>(grid[1][j])) / 3, false);
-        	grid[0][j]=new_grid[0][j];
+	// Left edge
+	for (int j = 1; j < grid_height - 1; ++j) {
+		if (get<1>(new_grid[0][j]) == false)
+			new_grid[0][j] = tuple<double, bool>((get<0>(new_grid[0][j - 1]) + get<0>(new_grid[0][j + 1]) + get<0>(new_grid[1][j])) / 3, false);
+
 	}
-        // Right edge
-        for (int j = 1; j < grid_height - 1; ++j) {
-            if (get<1>(grid[grid_width - 1][j]) == false)
-                new_grid[grid_width - 1][j] = tuple<double, bool>((get<0>(grid[grid_width - 1][j - 1]) + get<0>(grid[grid_width - 1][j + 1]) + get<0>(grid[grid_width - 2][j])) / 3, false);
-        	grid[grid_width-1][j]=new_grid[grid_width-1][j];
+	// Right edge
+	for (int j = 1; j < grid_height - 1; ++j) {
+		if (get<1>(new_grid[grid_width - 1][j]) == false)
+			new_grid[grid_width - 1][j] = tuple<double, bool>((get<0>(new_grid[grid_width - 1][j - 1]) + get<0>(new_grid[grid_width - 1][j + 1]) + get<0>(new_grid[grid_width - 2][j])) / 3, false);
 	}
-        
-        // Finally, update the interior cells
-        for (int i = 1; i < grid_width - 1; ++i) {
-            for (int j = 1; j < grid_height - 1; ++j) {
-                if (get<1>(grid[i][j]) == false)
-                    new_grid[i][j] = tuple<double, bool>((get<0>(grid[i][j - 1]) + get<0>(grid[i - 1][j]) + get<0>(grid[i + 1][j]) + get<0>(grid[i][j + 1])) / 4, false);
-		grid[i][j]=new_grid[i][j];
-            }
-        }
+
+	// Finally, update the interior cells
+	for (int i = 1; i < grid_width - 1; ++i) {
+		for (int j = 1; j < grid_height - 1; ++j) {
+			if (get<1>(new_grid[i][j]) == false)
+				new_grid[i][j] = tuple<double, bool>((get<0>(new_grid[i][j - 1]) + get<0>(new_grid[i - 1][j]) + get<0>(new_grid[i + 1][j]) + get<0>(new_grid[i][j + 1])) / 4, false);
+		}
+	}
+}
+
+
+// TODO: Performs one iteration of the SOR simulation with relaxation constant 'omega' ***************************************************************************
+// Author: Wenbo. Modified to work with converged().
+void Board::sorUpdate() {
+
+	int grid_width = grid.size();
+	int grid_height = grid[0].size();
+	double omega = 2 / (1 + 3.14159 / grid_width);
+
+	grid = new_grid;
+
+	// First update the corner pieces:
+
+	if (get<1>(grid[0][0]) == false)				// Top-left
+		new_grid[0][0] = tuple<double, bool>(0.5*(get<0>(grid[0][1]) + get<0>(grid[1][0])), false);
+
+	if (get<1>(grid[0][grid_height - 1]) == false)	// Bottom-left
+		new_grid[0][grid_height - 1] = tuple<double, bool>(0.5*(get<0>(grid[0][grid_height - 2]) + get<0>(grid[1][grid_height - 1])), false);
+
+	if (get<1>(grid[grid_width - 1][0]) == false)		// Top-right
+		new_grid[grid_width - 1][0] = tuple<double, bool>(0.5*(get<0>(grid[grid_width - 2][0]) + get<0>(grid[grid_width - 1][1])), false);
+
+	if (get<1>(grid[grid_width - 1][grid_height - 1]) == false)	// Bottom-right
+		new_grid[grid_width - 1][grid_height - 1] = tuple<double, bool>(0.5*(get<0>(grid[grid_width - 2][grid_height - 1]) + get<0>(grid[grid_width - 1][grid_height - 2])), false);
+
+
+	// Next, update the edge pieces:
+	// Top edge
+	for (int i = 1; i < grid_width - 1; ++i) {
+		if (get<1>(grid[i][0]) == false)
+			new_grid[i][0] = tuple<double, bool>((get<0>(grid[i - 1][0]) + get<0>(grid[i + 1][0]) + get<0>(grid[i][1])) / 3, false);
+	}
+	// Bottom edge
+	for (int i = 1; i < grid_width - 1; ++i) {
+		if (get<1>(grid[i][grid_height - 1]) == false)
+			new_grid[i][grid_height - 1] = tuple<double, bool>((get<0>(grid[i - 1][grid_height - 1]) + get<0>(grid[i + 1][grid_height - 1]) + get<0>(grid[i][grid_height - 2])) / 3, false);
+	}
+	// Left edge
+	for (int j = 1; j < grid_height - 1; ++j) {
+		if (get<1>(grid[0][j]) == false)
+			new_grid[0][j] = tuple<double, bool>((get<0>(grid[0][j - 1]) + get<0>(grid[0][j + 1]) + get<0>(grid[1][j])) / 3, false);
+	}
+	// Right edge
+	for (int j = 1; j < grid_height - 1; ++j) {
+		if (get<1>(grid[grid_width - 1][j]) == false)
+			new_grid[grid_width - 1][j] = tuple<double, bool>((get<0>(grid[grid_width - 1][j - 1]) + get<0>(grid[grid_width - 1][j + 1]) + get<0>(grid[grid_width - 2][j])) / 3, false);
+	}
+
+	// Finally, update the interior cells
+	for (int i = 1; i < grid_width - 1; ++i) {
+		for (int j = 1; j < grid_height - 1; ++j) {
+			if (get<1>(grid[i][j]) == false)
+				new_grid[i][j] = tuple<double, bool>((1 - omega)*get<0>(grid[i][j]) + omega*(get<0>(new_grid[i][j - 1]) + get<0>(new_grid[i - 1][j]) + get<0>(grid[i + 1][j]) + get<0>(grid[i][j + 1])) / 4, false);
+		}
+	}
 }
 
 	
@@ -207,7 +259,7 @@ void Board::writeBoard(string ofilename) {
 	ofile.open(ofilename);
 	for (int i = 0; i < grid_width; ++i) {
 		for (int j = 0; j < grid_height; ++j) {
-			ofile << i << " " << j << " " << get<0>(grid[i][j]) << endl;
+			ofile << i << " " << j << " " << get<0>(new_grid[i][j]) << endl;
 		}
 		ofile << endl;
 	}
